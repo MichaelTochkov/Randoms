e162b9332211a36b7587c8d6ff8c08ccc5f78a18
MichaelTochkov <61181755+MichaelTochkov@users.noreply.github.com>
2021-11-08 16:34:49 +0200

    random

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Randomss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] arr = new int[10];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(1, 17);
+            }
+            foreach (var item in arr)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
